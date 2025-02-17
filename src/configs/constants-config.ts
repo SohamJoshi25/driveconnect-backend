a055012ed01b5182a484045303bf4f13beedff37
Soham Joshi <sohamjoshichinchwad@gmail.com>
2025-02-17 23:45:30 +0530

    Google Redirect 2

--- src/configs/constants-config.ts
+++ src/configs/constants-config.ts
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT ?? 8080;
 export const SERVER_DOMAIN = process.env.NODE_ENV === "DEVELOPMENT" ? ("http://localhost:" + PORT) : "https://driveconnect-backend.onrender.com";
-export const GOOGLE_REDIRECT_URL = "http://localhost:8080" + "/v1/auth/google/callback";
+export const GOOGLE_REDIRECT_URL = SERVER_DOMAIN + "/v1/auth/google/callback";
 export const APP_DOMAIN = process.env.NODE_ENV === "DEVELOPMENT" ? "http://localhost:5173" : "https://driveconnect-app.vercel.app";
