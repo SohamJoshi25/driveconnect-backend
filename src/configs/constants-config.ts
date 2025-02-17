c39d6d74c8de6d929560d293fa34639c48f503eb
Soham Joshi <sohamjoshichinchwad@gmail.com>
2025-02-17 23:35:11 +0530

    domain

--- src/configs/constants-config.ts
+++ src/configs/constants-config.ts
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT ?? 8080;
-export const SERVER_DOMAIN = process.env.NODE_ENV === "DEVELOPMENT" ? "http://localhost:" + PORT : "https://sterling-feline-glowing.ngrok-free.app";
+export const SERVER_DOMAIN = process.env.NODE_ENV === "DEVELOPMENT" ? "http://localhost:" + PORT : "https://driveconnect-backend.onrender.com";
 export const GOOGLE_REDIRECT_URL = "http://localhost:8080" + "/v1/auth/google/callback";
-export const APP_DOMAIN = process.env.NODE_ENV === "DEVELOPMENT" ? "http://localhost:5173" : "http://localhost:5173";
+export const APP_DOMAIN = process.env.NODE_ENV === "DEVELOPMENT" ? "http://localhost:5173" : "https://driveconnect-app.vercel.app";
