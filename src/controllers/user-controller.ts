6c5b821e1b576e9efc47340d8dadcdf28115448b
Soham Joshi <sohamjoshichinchwad@gmail.com>
2025-02-19 12:12:56 +0530

    UserNotFound

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -10,6 +10,9 @@ export const UserInfo = async (request: Request, response: Response): Promise<an
   try {
     const userId = request.session.userId;
     const user = await UserModel.findById(userId);
+    if(!user){
+      return response.status(404).json({mesage:"No User Found"})
+    }
     return response.status(200).json({ message: "Success", user: user });
   } catch (error) {
     console.error(error);
