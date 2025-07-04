42d766022b934af15fda5501edd8e3534db165af
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-07-04 10:54:22 +0530

    changed appname domain to SohamJoshi.in

--- src/configs/constants-config.ts
+++ src/configs/constants-config.ts
@@ -4,4 +4,4 @@ dotenv.config();
 export const PORT = process.env.PORT ?? 8080;
 export const SERVER_DOMAIN = process.env.NODE_ENV == "DEVELOPMENT" ? ("http://localhost:" + PORT) : "https://driveconnect-backend.onrender.com";
 export const GOOGLE_REDIRECT_URL = SERVER_DOMAIN + "/v1/auth/google/callback";
-export const APP_DOMAIN = process.env.NODE_ENV == "DEVELOPMENT" ? "http://localhost:5173" : "https://driveconnect-app.vercel.app";
+export const APP_DOMAIN = process.env.NODE_ENV == "DEVELOPMENT" ? "http://localhost:5173" : "https://driveconnect.sohamjoshi.in";
